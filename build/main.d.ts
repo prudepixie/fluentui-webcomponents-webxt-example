4371d75ac683258754e5ccedf1bf5f91ec2543f7
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-03-09 16:36:15 -0800

    Updates

---
+++ build/main.d.ts
@@ -0,0 +1,9 @@
+import { FASTElement, ViewTemplate } from "@microsoft/fast-element";
+export declare class ComponentList extends FASTElement {
+    repeatNum: number;
+    component: string | null;
+    templatestr: string;
+    componentTemplate: ViewTemplate;
+    connectedCallback(): void;
+}
+//# sourceMappingURL=main.d.ts.map
